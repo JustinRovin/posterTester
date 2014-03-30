39c92bdd39cad7aaf3d7e9623e0ed09cce26671b
Justin Rovin <justinrovin@gmail.com>
2014-03-30 04:34:56 -0700

    yup

---
+++ aspec.py
@@ -0,0 +1,18 @@
+import math
+
+file = open('rando3')
+lines = file.readlines()
+file.close()
+totDist = 0
+sacCount = 0
+totFix = 0
+fixCount = 0
+
+array = ["hello", 2l, 1]
+print array.split()
+
+
+
+#int(line[4])/1000000
+
+
